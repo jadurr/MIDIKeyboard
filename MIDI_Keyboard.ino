786bb883224e47d9a6b9c2e48720bd2e363ddce2
Julian Durr <43624274+jadurr@users.noreply.github.com>
2018-12-20 13:55:46 -0500

    Added MIDI Keyboard Files

---
+++ MIDI_Keyboard.ino
@@ -0,0 +1,45 @@
+#include "MIDIUSB.h"
+#include "PitchToNote.h"
+
+const int selectPins[3] = {2, 3, 4};
+const int digitalMuxInput = 5;
+const int analogMuxInput = A0;
+const int digitalInputs[4] = {6, 7, 8, 9};
+
+void setup() {
+  for(int i=0; i<3; i++){
+    pinMode(selectPins[i],OUTPUT);
+  }
+  for(int i=0; i<4; i++){
+    pinMode(digitalInputs[i], INPUT_PULLUP);
+  }
+  pinMode(digitalMuxInput, INPUT_PULLUP);
+  pinMode(analogMuxInput, INPUT);
+}
+
+void loop() {
+
+}
+
+void selectMuxPin(byte pin){
+  for(int i=0; i<3; i++){
+    if(pin & (1<<i))
+      digitalWrite(selectPins[i], HIGH);
+    else
+      digitalWrite(selectPins[i], LOW);
+  }
+}
+void noteOn(byte channel, byte pitch, byte velocity) {
+  midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
+  MidiUSB.sendMIDI(noteOn);
+}
+
+void noteOff(byte channel, byte pitch, byte velocity) {
+  midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, velocity};
+  MidiUSB.sendMIDI(noteOff);
+}
+
+void controlChange(byte channel, byte control, byte value) {
+  midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
+  MidiUSB.sendMIDI(event);
+}
